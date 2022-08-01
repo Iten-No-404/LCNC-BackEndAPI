8f7ff7313433aab3f9b5a14df56408b5b3340fc9
Mohamed Ahmed <mohamed.sayed001@eng-st.cu.edu.eg>
2022-08-01 21:01:36 +0200

    add connection string and connect to sql

--- Itworx-Backend/Itworx-Backend/Program.cs
+++ Itworx-Backend/Itworx-Backend/Program.cs
@@ -1,6 +1,10 @@
+using Itworx_Backend.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(item => item.UseSqlServer(builder.Configuration.GetConnectionString("myconn")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +19,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseAuthorization();
 
