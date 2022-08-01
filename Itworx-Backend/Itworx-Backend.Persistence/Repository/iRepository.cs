0ff5631b16c39d1f39f1d1bdcb9db18ff29919ac
Mohamed Ahmed <mohamed.sayed001@eng-st.cu.edu.eg>
2022-08-01 21:20:52 +0200

    layer 2 done

---
+++ Itworx-Backend/Itworx-Backend.Persistence/Repository/iRepository.cs
@@ -0,0 +1,20 @@
+﻿using Itworx_Backend.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itworx_Backend.Persistence.Repository
+{
+    public interface iRepository < T > where T: baseEntity
+    {
+        IEnumerable<T> GetAll();
+        T Get(int Id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Remove(T entity);
+        void SaveChanges();
+    }
+}
