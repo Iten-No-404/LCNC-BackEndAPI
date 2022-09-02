5aa913c38459f7090cbac46dae600c3f5c604d12
Iten Elhak <iten_emad@yahoo.com>
2022-09-02 17:47:28 +0200

    Upload Image Modification
    Now, the UploadImage Route, creates a directory if file path is given and saves the image inside it.

--- Itworx-Backend/Itworx-Backend/Controllers/FileController.cs
+++ Itworx-Backend/Itworx-Backend/Controllers/FileController.cs
@@ -29,12 +29,43 @@ namespace Itworx_Backend.Controllers
         {
             try
             {
-                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", file.ImageName);
+                string path = "";
+                Console.WriteLine("################File Path:"+ file.ImagePath);
+                Console.WriteLine("################File Name:" + file.ImageName);
+                if (file.ImagePath != "")
+                {
+                    path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/", file.ImagePath);
+                    try
+                    {
+                        // Determine whether the directory exists.
+                        if (Directory.Exists(path))
+                        {
+                            Console.WriteLine("That path exists already.");
+                        }
+                        else
+                        {
+
+                        // Try to create the directory.
+                        DirectoryInfo di = Directory.CreateDirectory(path);
+                        Console.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime(path));
+                        }
+                        path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/", file.ImagePath, file.ImageName);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("The process failed: {0}", e.ToString());
+                    }
+                }
+                else
+                    path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", file.ImageName);
                 using (Stream stream = new FileStream(path, FileMode.Create))
                 {
                     file.Image.CopyTo(stream);
                 }
-                file.ImagePath = "localhost:5053/images/" + file.ImageName;
+                if (file.ImagePath != "")
+                    file.ImagePath = "localhost:5053/images/" + file.ImagePath + file.ImageName;
+                else
+                    file.ImagePath = "localhost:5053/images/" + file.ImageName;
                 _ImageService.Insert(file);
                 return Ok(file);
             }
